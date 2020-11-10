5defc7467b0c415b42a23843911ccab213f18d37
George Yoo <gcyoosf@gmail.com>
2020-11-09 20:48:48 -0500

    modified card component with static data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,12 @@ class Card extends Component {
     render() {
         return (
             <div>
-                <p> I'm a Card!</p>
+                <h3 className="Card-name">Squirtle</h3>
+                <img src="#" alt="Pokemon Image" className="Card-img" />
+                <p className="Card-type">Type: Water</p>
+                <p className="Card-hp">HP: 60</p>
+                <p className="Card-ap">AP: 10</p>
+                <p className="Card-dp">DP: 15</p>
             </div>
         )
     }
