be4ea24b357f43b709542c686ca6aeae78dd7b73
George Yoo <gcyoosf@gmail.com>
2020-11-09 20:42:19 -0500

    removes default react files and adds a Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Card.css'
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+                <p> I'm a Card!</p>
+            </div>
+        )
+    }
+}
+
+export default Card
